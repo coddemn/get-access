a2514285437ae3525d5c79f50fcb789cc426dd20
aCup_of_Tea <konovalov.dev@yandex.ru>
2026-09-22 22:08:32 +0300

    feet: add main package

---
+++ cmd/app/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/coddemn/get-access/internal/config"
+	"github.com/coddemn/get-access/internal/database"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	//===============================================
+	//	LOAD CONFIGS
+	//===============================================
+
+	_ = godotenv.Load(".env")
+
+	ctx := context.Background()
+
+	cfg, err := config.Load("configs/config.yaml")
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+	}
+
+	//======================
+	//	CONNECT TO DB
+	//======================
+
+	database, err := database.New(ctx, cfg.DB)
+	if err != nil {
+		log.Fatalf("init db: %v", err)
+	}
+	defer func() {
+		database.Close()
+		log.Println("db connection is closed")
+	}()
+
+	log.Println("connected to database")
+
+	//=======================
+	//	INITIALYZE
+	//=======================
+
+	httpAddr := fmt.Sprintf("%s:%s", cfg.Server.Host, cfg.Server.Port)
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	httpServer := &http.Server{
+		Addr:    httpAddr,
+		Handler: router,
+	}
+
+	//==============================
+	//	ENDPOINTS
+	//==============================
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hi from get-access, Demidos!",
+		})
+	})
+
+	//===============================
+	//	START SERVER
+	//===============================
+
+	go func() {
+		log.Printf("HTTP Server is running on %s", httpAddr)
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("HTTP Server error: %v", err)
+		}
+	}()
+
+	//=================================
+	//	GRACEFUL SHUTDOWN (CTRL+C)
+	//=================================
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigChan
+	log.Printf("Given signal %v. Stopping work...\n", sig)
+
+	shtdCtx, shtdCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shtdCancel()
+
+	if err := httpServer.Shutdown(shtdCtx); err != nil {
+		log.Printf("HTTP shutdown error: %v", err)
+	}
+
+	// wait workers is finalized
+	//wg.Wait()
+	log.Println("All workers is completed. App correct stopped.")
+
+}
